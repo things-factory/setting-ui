a39d37f472274afa287e60927793f019f5af8404
horwengliang <horwengliang@gmail.com>
2019-05-03 16:13:50 +0800

    Add Email as Setting Header

--- src/pages/setting.js
+++ src/pages/setting.js
@@ -1,20 +1,44 @@
-import { html } from 'lit-element'
+import { html, css } from 'lit-element'
 import { connect } from 'pwa-helpers/connect-mixin.js'
 import { store, PageView } from '@things-factory/shell'
+
 // import { i18next } from '@things-factory/shell/base/i18next'
 
 class SettingUiMain extends connect(store)(PageView) {
   static get properties() {
     return {
-      settingUi: String
+      _email: String
     }
   }
+
+  static get styles() {
+    return [
+      css`
+        div {
+          text-align: center;
+          background-color: #e5e5e5;
+          height: 50px;
+          padding: 20px 0px 20px 0px;
+        }
+
+        p {
+          margin: 0;
+        }
+      `
+    ]
+  }
+
   render() {
     return html`
-      <section>
-        XXXXXX
-      </section>
+      <div>
+        <p><b>Admin</b></p>
+        <p>${this._email}</p>
+      </div>
     `
+  }
+
+  stateChanged(state) {
+    this._email = state.auth.user.email
   }
 
   get context() {
