4458a48b6c38939431a36eb2e6c6fcfdc218db21
horwengliang <horwengliang@gmail.com>
2019-05-06 11:09:24 +0800

    Add Sorted Setting & Remove CSS

--- src/pages/setting.js
+++ src/pages/setting.js
@@ -1,4 +1,4 @@
-import { html, css } from 'lit-element'
+import { html } from 'lit-element'
 import { connect } from 'pwa-helpers/connect-mixin.js'
 import { store, PageView } from '@things-factory/shell'
 
@@ -7,60 +7,27 @@ import { store, PageView } from '@things-factory/shell'
 class SettingUiMain extends connect(store)(PageView) {
   static get properties() {
     return {
-      _email: String,
       _settings: Array
     }
   }
 
-  static get styles() {
-    return [
-      css`
-        #settingHeader {
-          background-color: #e5e5e5;
-          height: 50px;
-          padding: 20px 0px 20px 0px;
-          text-align: center;
-        }
-
-        p {
-          margin: 0;
-        }
-
-        #languageSetting {
-          padding: 30px;
-        }
-
-        span {
-          color: #c0504d;
-          text-align: left;
-          font-size: 1em;
-        }
-      `
-    ]
-  }
-
   render() {
-    return html`
-      <div id="settingHeader">
-        <p><b>Admin</b></p>
-        <p>${this._email}</p>
-      </div>
+    var _sortedSettings = this._settings.sort((a, b) => {
+      return a.seq - b.seq
+    })
 
-      <div id="languageSetting">
-        <span>Change Language</span>
-        <div>
-          ${this._settings.map(
-            setting => html`
-              ${setting.template}
-            `
-          )}
-        </div>
+    return html`
+      <div>
+        ${_sortedSettings.map(
+          setting => html`
+            ${setting.template}
+          `
+        )}
       </div>
     `
   }
 
   stateChanged(state) {
-    this._email = state.auth.user && state.auth.user.email
     this._settings = state.setting.settings
   }
 
