fab7eec9ffe083a632839509c7eeed66a22f3a12
Murky <moosung.gil@hatiolab.com>
2019-12-11 12:01:19 +0900

    static auth pages
    related to things-factory/shell#21

--- client/setting-lets/domain-switch-let.js
+++ client/setting-lets/domain-switch-let.js
@@ -1,8 +1,8 @@
-import { LitElement, html } from 'lit-element'
-import { connect } from 'pwa-helpers/connect-mixin'
-import { store, navigate } from '@things-factory/shell'
 import '@things-factory/i18n-base'
 import '@things-factory/setting-base'
+import { store } from '@things-factory/shell'
+import { html, LitElement } from 'lit-element'
+import { connect } from 'pwa-helpers/connect-mixin'
 
 export class DomainSwitchLet extends connect(store)(LitElement) {
   static get properties() {
@@ -18,7 +18,7 @@ export class DomainSwitchLet extends connect(store)(LitElement) {
       <setting-let>
         <i18n-msg slot="title" msgid="title.switch domain"></i18n-msg>
 
-        <select slot="content" @change=${e => navigate('/domain/' + e.target.value)}>
+        <select slot="content" @change=${e => (window.location.pathname = '/checkin/' + e.target.value)}>
           ${this.domains.map(
             option => html`
               <option value=${option.subdomain} ?selected=${this.subdomain == option.subdomain}>${option.name}</option>
